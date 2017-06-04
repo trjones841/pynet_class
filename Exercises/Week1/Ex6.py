4d9dbf43e4cc13529fc98317fb2e602dd749fa51
trjones841 <terry.jones@war-eagle.me>
2017-06-04 12:53:31 -0700

    Add Ex6 and Ex7

--- Exercises/Week1/Ex6.py
+++ Exercises/Week1/Ex6.py
@@ -5,5 +5,34 @@ Create a list. One element of list should be a dictionary with min 2 keys.
 Write out list using both Yaml and JSON formats.
 
 '''
+import yaml
+import json
 
-NEW_LIST = ('')
+def main():
+
+    yamlfile = 'data.yml'
+    jsonfile = 'data.json'
+
+    dict_one = {
+        'one' : 'uno',
+        'two' : 'dos'
+    }
+    dict_two = {
+        'three': 'tres',
+        'four' : 'cuatro',
+        'five' : 'cinco'
+    }
+    NEW_LIST = ['one', 'two', 'three', 'four', dict_one, 'five', dict_two]
+
+    with open(yamlfile, "w") as yf:
+        #print yaml.dump(NEW_LIST, default_flow_style=False)
+        yf.write(yaml.dump(NEW_LIST, default_flow_style=False))
+
+    with open(jsonfile, "w") as jf:
+        #print json.dumps(NEW_LIST, sort_keys=True, indent=4, separators=(',', ':'))
+        jf.write(json.dumps(NEW_LIST, sort_keys=True, indent=4, separators=(',', ':')))
+
+    return True
+
+if __name__ == '__main__':
+    main()
---
+++ Exercises/Week1/Ex7.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.7
+
+'''
+Read in lists from Ex6 and ensure pretty print the struct
+'''
+
+
+def main():
+
+    yamlfile = 'data.yml'
+    jsonfile = 'data.json'
+
+    yfile = open(yamlfile, "r").read().splitlines()
+    for line in yfile:
+        print line
+
+    jfile = open(jsonfile, "r").read().splitlines()
+    for line in jfile:
+        print line
+
+if __name__ == '__main__':
+    main()
